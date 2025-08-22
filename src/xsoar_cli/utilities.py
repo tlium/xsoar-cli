bfc0dd9967e10750e0b3a1461fea6e7214b9a391
Torbjørn Lium <torben@lium.org>
2025-08-22 20:06:10 +0200

    Small improvements

--- src/xsoar_cli/utilities.py
+++ src/xsoar_cli/utilities.py
@@ -14,7 +14,7 @@ def parse_string_to_dict(input_string: str | None, delimiter: str) -> dict:
     # Parse a string into a python dictionary
     pairs = [pair.split("=", 1) for pair in input_string.split(delimiter)]
     # Filter pairs that have exactly 2 parts (key and value) after splitting by "="
-    valid_pairs = [pair for pair in pairs if len(pair) == 2]
+    valid_pairs = [pair for pair in pairs if len(pair) == 2]  # noqa: PLR2004
     return {key.strip(): value.strip() for key, value in valid_pairs}
 
 
