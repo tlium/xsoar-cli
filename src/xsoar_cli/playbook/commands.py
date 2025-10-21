4aba384d59339ff647fc520e6f7520c87a3c9302
Torbjørn Lium <torben@lium.org>
2025-10-21 12:31:00 +0200

    Fetch xsoar-client from proper location in ctx.obj
    Forgot to fix this after the refactor a while back. Works now.

--- src/xsoar_cli/playbook/commands.py
+++ src/xsoar_cli/playbook/commands.py
@@ -19,7 +19,9 @@ def playbook(ctx: click.Context) -> None:
     """Download/attach/detach playbooks"""
 
 
-@click.option("--environment", default=None, help="Default environment set in config file.")
+@click.option(
+    "--environment", default=None, help="Default environment set in config file."
+)
 @click.command()
 @click.argument("name", type=str)
 @click.pass_context
@@ -34,7 +36,7 @@ def download(ctx: click.Context, environment: str | None, name: str) -> None:
     """
     if not environment:
         environment = ctx.obj["default_environment"]
-    xsoar_client: Client = ctx.obj["server_envs"][environment]
+    xsoar_client: Client = ctx.obj["server_envs"][environment]["xsoar_client"]
     # Maybe we should search for the playbook before attempting download in
     # case user specifies a cutsom playbook and not a system playbook
     try:
@@ -46,20 +48,34 @@ def download(ctx: click.Context, environment: str | None, name: str) -> None:
         sys.exit(1)
     playbook_bytes_data = StringIO(playbook.decode("utf-8"))
     playbook_data = yaml.safe_load(playbook_bytes_data)
-    pack_id = playbook_data["contentitemexportablefields"]["contentitemfields"]["packID"]
+    pack_id = playbook_data["contentitemexportablefields"]["contentitemfields"][
+        "packID"
+    ]
     cwd = pathlib.Path().cwd()
     target_dir = pathlib.Path(cwd / "Packs" / pack_id / "Playbooks")
     if not target_dir.is_dir():
         msg = f"Cannot find target directory: {target_dir}\nMaybe you're not running xsoar-cli from the root of a content repository?"
         click.echo(msg)
         sys.exit(1)
-    filepath = pathlib.Path(cwd / "Packs" / pack_id / "Playbooks" / f"{playbook_data['id']}.yml")
+    filepath = pathlib.Path(
+        cwd / "Packs" / pack_id / "Playbooks" / f"{playbook_data['id']}.yml"
+    )
     filepath = pathlib.Path(str(filepath).replace(" ", "_"))
     with filepath.open("w") as f:
         yaml.dump(playbook_data, f, default_flow_style=False)
     click.echo(f"Written playbook to: {filepath}")
     click.echo("Running demisto-sdk format on newly downloaded playbook")
-    subprocess.run(["demisto-sdk", "format", "--assume-yes", "--no-validate", "--no-graph", str(filepath)], check=False)  # noqa: S603, S607
+    subprocess.run(
+        [
+            "demisto-sdk",
+            "format",
+            "--assume-yes",
+            "--no-validate",
+            "--no-graph",
+            str(filepath),
+        ],
+        check=False,
+    )  # noqa: S603, S607
     click.echo("Re-attaching playbook in XSOAR...", nl=False)
     xsoar_client.attach_item(item_type="playbook", item_id=name)
     click.echo("done.")
