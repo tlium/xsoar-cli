51301365ceac42b3e04e6db6329b0d6da42b3258
Torbjørn Lium <torben@lium.org>
2026-01-24 21:32:29 +0100

    Fix unit test

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,4 @@
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, PropertyMock, patch
 
 import pytest
 from click.testing import CliRunner
@@ -7,8 +7,7 @@ from xsoar_cli import cli
 
 
 class TestConfig:
-    # @pytest.mark.usefixtures("mock_config_file")
-    @patch("xsoar_client.xsoar_client.Client.test_connectivity", MagicMock(return_value=True))
+    @patch("xsoar_cli.configuration.Client")
     @patch("pathlib.Path.is_file", MagicMock(return_value=True))
     @pytest.mark.parametrize(
         ("cli_args", "use_fixtures", "expected_return_value"),
@@ -18,10 +17,18 @@ class TestConfig:
             # (["config", "get"], True, 0),
         ],
     )
-    def test_config(self, cli_args: list[str], use_fixtures: bool, expected_return_value: int, request: pytest.FixtureRequest) -> None:  # noqa: FBT001
+    def test_config(
+        self, mock_client, cli_args: list[str], use_fixtures: bool, expected_return_value: int, request: pytest.FixtureRequest
+    ) -> None:  # noqa: FBT001
+        mock_instance = MagicMock()
+        mock_instance.test_connectivity.return_value = True
+        mock_provider = MagicMock()
+        mock_provider.test_connection.return_value = True
+        mock_instance.artifact_provider = mock_provider
+        mock_client.return_value = mock_instance
+
         if use_fixtures:
             mock_config_file = request.getfixturevalue("mock_config_file")  # noqa: F841
         runner = CliRunner()
         result = runner.invoke(cli.cli, cli_args)
         assert result.exit_code == expected_return_value
-        print(result.output)
