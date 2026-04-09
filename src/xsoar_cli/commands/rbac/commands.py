a1df6bda25138d3f762a10dccc5ccdeca9a8d7d4
Torbjørn Lium <torben@lium.org>
2026-04-09 22:17:13 +0200

    Remove trailing newline from RBAC JSON output

--- src/xsoar_cli/commands/rbac/commands.py
+++ src/xsoar_cli/commands/rbac/commands.py
@@ -29,7 +29,7 @@ def getroles(ctx: click.Context, environment: str | None) -> None:
     config = get_xsoar_config(ctx)
     xsoar_client: Client = config.get_client(environment)
     roles = xsoar_client.rbac.get_roles()
-    click.echo(json.dumps(roles, sort_keys=True, indent=4) + "\n")
+    click.echo(json.dumps(roles, sort_keys=True, indent=4))
 
 
 @click.command()
@@ -42,7 +42,7 @@ def getusers(ctx: click.Context, environment: str | None) -> None:
     config = get_xsoar_config(ctx)
     xsoar_client: Client = config.get_client(environment)
     users = xsoar_client.rbac.get_users()
-    click.echo(json.dumps(users, sort_keys=True, indent=4) + "\n")
+    click.echo(json.dumps(users, sort_keys=True, indent=4))
 
 
 @click.command()
@@ -58,7 +58,7 @@ def getusergroups(ctx: click.Context, environment: str | None) -> None:
         click.echo("Error: Command not supported for XSOAR server versions less than 8")
         ctx.exit(1)
     user_groups = xsoar_client.rbac.get_user_groups()
-    click.echo(json.dumps(user_groups, sort_keys=True, indent=4) + "\n")
+    click.echo(json.dumps(user_groups, sort_keys=True, indent=4))
 
 
 rbac.add_command(getroles)
